a8f211d888542e5a302bfbb43847bd4351cd3775
HarshitaAgarwal09 <harshitaagarwal98@yahoo.com>
2020-07-24 17:14:36 +0530

    .

--- index.js
+++ index.js
@@ -9,17 +9,9 @@ const app = express();
 //nodejs
 const server = require("http").Server(app);
 
-//nodejs enable socket
-const io = require("socket.io")(server);
 
 // serve static assets to client
 app.use(express.static("src"));
-
-//server
-io.on("connect", function (socket) {
-    console.log(socket.id);
-    //rest code;////////////////////////
-})
 
 //nodejs server
 const port = process.env.PORT || 3000;
